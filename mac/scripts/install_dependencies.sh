c3989f92120a1cf2692d3f4e4e88cb4371999771
ben-smyth <ben.df.smyth@gmail.com>
2024-06-09 21:16:11 +0100

    first commit

---
+++ mac/scripts/install_dependencies.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# 1. install brew
+/usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+
+# 2. install using brewfile
