23bb896f70839802c471424838cefa263362a0a5
ben-smyth <ben.df.smyth@gmail.com>
2024-06-12 17:35:12 +0100

    tweaks to wsl

--- wsl/scripts/install_dependencies.sh
+++ wsl/scripts/install_dependencies.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+# apt package updates and upgrades
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
 # install brew
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/bsmyth/.bashrc
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+source ~/.bashrc
 
 # set basic git settings
-git config --global user.email "ben@bensmyth.me"
-git config --global user.name "Ben Smyth"
+git config --global user.email "ben.df.smyth@gmail.com"
+git config --global user.name "ben-smyth"
 
 # clone the full repo for dotfiles and dependencies
-git clone https://github.com/ben-smyth/installs_bs.git
+git clone https://github.com/ben-smyth/installs_bs.git ~/installs_bs
