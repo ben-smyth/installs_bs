6ef2f5f4117ee45dd15aed8ce3abdf424d8689a4
Ben Smyth <ben.smyth@eu.equinix.com>
2024-06-25 14:43:47 +0100

    adding tmux multissh function

--- dotfiles/.ben_scripts.sh
+++ dotfiles/.ben_scripts.sh
@@ -1,3 +1,17 @@
+multissh() {
+    local HOSTS=($@)
+    if [ -z "$HOSTS" ]; then
+       echo -n "Please provide a list of hosts separated by spaces [ENTER]: "
+       read -A HOSTS
+    fi
+
+    tmux new-window "ssh ${HOSTS[1]};"
+    printf '%s\n' "${HOSTS[@]:1}" | xargs -I {} tmux split-window -h "ssh {};"
+    tmux select-layout tiled > /dev/null
+    tmux select-pane -t 0
+    tmux set-window-option synchronize-panes on > /dev/null
+}
+
 updateDotFiles() {
     # function to update my dotfiles by git pulling and running install
     git -C ${HOME}/installs_bs pull
