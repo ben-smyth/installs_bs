d1f048f72888241700c2c44f3824417d590b3ae8
Ben Smyth <ben.smyth@eu.equinix.com>
2024-06-25 14:36:38 +0100

    trying to fix nvim weirdness

---
+++ dotfiles/.config/nvim2/lua/custom/plugins/filetree.lua
@@ -0,0 +1,18 @@
+return {
+"nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = true,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {
+  view = {
+    width = 30,
+  },
+  filters = {
+    dotfiles = true,
+  },
+}
+  end,
+}
